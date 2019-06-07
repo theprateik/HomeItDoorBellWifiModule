19cd9ab5778ae141596b55832c09eda80ef4ff0f
P.shrestha <molly.shrestha@gmail.com>
2019-06-06 20:49:03 -0400

    code format

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <ESP8266WiFi.h>
 #include "SpiffsFunctions.h"
 #include "WebServerFunctions.h"
@@ -13,46 +12,46 @@ int outputPin = LED_BUILTIN;
 
 void setup()
 {
-  pinMode(inputPin, INPUT);
-  pinMode(outputPin, OUTPUT);
+	pinMode(inputPin, INPUT);
+	pinMode(outputPin, OUTPUT);
 
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  WiFi.disconnect();
-  WiFi.softAPdisconnect();
+	WiFi.disconnect();
+	WiFi.softAPdisconnect();
 
-  startSPIFFS();
-  startNetwork();
-  startWebServer();
+	startSPIFFS();
+	startNetwork();
+	startWebServer();
 
-  if (NETWORK_AS_STATION == true)
-  {
-    Serial.println("Staring the Hub");
-    startHub();
-  }
+	if (NETWORK_AS_STATION == true)
+	{
+		Serial.println("Staring the Hub");
+		startHub();
+	}
 }
 
 void loop()
 {
-  if (NETWORK_AS_AP == true)
-  {
-    WebSocketServer.loop();
-  }
-  else if (NETWORK_AS_STATION == true)
-  {
-    int sensorValue = digitalRead(inputPin);
-    websocketClientLoop();
-    if (millis() > time_now + period && sensorValue > 0)
-    {
-        time_now = millis();
+	if (NETWORK_AS_AP == true)
+	{
+		WebSocketServer.loop();
+	}
+	else if (NETWORK_AS_STATION == true)
+	{
+		int sensorValue = digitalRead(inputPin);
+		websocketClientLoop();
+		if (millis() > time_now + period && sensorValue > 0)
+		{
+			time_now = millis();
 
-        Serial.println(sensorValue);
+			Serial.println(sensorValue);
         
-        digitalWrite(outputPin, LOW);
+			digitalWrite(outputPin, LOW);
         
-        ringBell();
-    }
-    digitalWrite(outputPin, HIGH);
-  }
-  WebServer.handleClient();
+			ringBell();
+		}
+		digitalWrite(outputPin, HIGH);
+	}
+	WebServer.handleClient();
 }
